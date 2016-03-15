44c11990b328f6f5b4b444b924a458bdcb9bc14f
zengzhiwen <zhiwen.zeng@gmail.com>
2016-03-15 19:52:28 +0800

    split dex task

--- app/src/main/java/com/github/zzwwws/plugig/MainActivity.java
+++ app/src/main/java/com/github/zzwwws/plugig/MainActivity.java
@@ -4,13 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.github.zzwwws.plugig.plugin.ActivityA;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityA.start(MainActivity.this);
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/github/zzwwws/plugig/plugin/ActivityA.java
@@ -0,0 +1,24 @@
+package com.github.zzwwws.plugig.plugin;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.github.zzwwws.plugig.R;
+
+/**
+ * Created by zzwwws on 2016/3/15.
+ */
+public class ActivityA extends AppCompatActivity {
+
+    public static void start(Context context){
+        Intent intent = new Intent(context, ActivityA.class);
+        context.startActivity(intent);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a);
+    }
+}
